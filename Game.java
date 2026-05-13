bf66f45053f0ed0b964e359a39c059c0b3352fca
Abdullah Ghafoor <143638785+astroverse-07@users.noreply.github.com>
2026-05-13 21:18:33 +0500

    Update Game.java
    Added Distance formula for Ghost to move smartly

--- Game.java
+++ Game.java
@@ -5,21 +5,42 @@ public class Game {
     public static void main(String[] args) {
 
         GameBoard board = new GameBoard();
+
+        // add Walls
+        board.addWall(3, 3);
+        board.addWall(3, 4);
+        board.addWall(3, 5);
+        board.addWall(6, 1);
+        board.addWall(6, 2);
+        board.addWall(8,3);
+        board.addWall(8,4);
+        board.addWall(8,5);
+        board.addWall(1,6);
+        board.addWall(1,7);
+        board.addWall(1,8);
+        board.addWall(7,7);
+        board.addWall(7,8);
+        board.addWall(7,9);
+
+
         Pacman pacman = new Pacman(0, 0);
         Ghost ghost = new Ghost(5, 5);
 
-        ArrayList<Food> foods = new ArrayList<>();
-
         //add food
+        ArrayList<Food> foods = new ArrayList<>();
         foods.add(new Food(2, 2));
-        foods.add(new Food(3, 7));
+        foods.add(new Food(4, 7));
         foods.add(new Food(8, 1));
+        foods.add(new Food(9, 9));
+        foods.add(new Food(6,5));
+        foods.add(new Food(7,2));
 
         Scanner sc = new Scanner(System.in);
         int score = 0;
 
-        while (true) {
+        System.out.println("--- PACMAN GAME START ---");
 
+        while (true) {
             board.initializeBoard();
 
             // place food
@@ -36,33 +57,35 @@ public class Game {
 
             //check collision ghost
             if (pacman.x == ghost.x && pacman.y == ghost.y) {
-                System.out.println("Game Over! Ghost caught Pacman.");
+                System.out.println("GAME OVER! Ghost caught Pacman.");
                 break;
             }
 
-            //Check food
+            //check food
             for (Food f : foods) {
                 if (!f.eaten && f.x == pacman.x && f.y == pacman.y) {
                     f.eaten = true;
                     score++;
+                    if (score == foods.size()) {
+                        System.out.println("YOU WIN! All food collected.");
+                        break;
+                    }
                 }
             }
 
-            if (score == foods.size()) {
-                System.out.println("You Win! All food collected.");
-                break;
-            }
+            if (score == foods.size()) break;
 
-            System.out.print("Move (WASD): ");
-            char move = sc.next().charAt(0);
+            System.out.print("Move (W/A/S/D): ");
+            char move = sc.next().toLowerCase().charAt(0);
 
             pacman.move(move, board);
-            ghost.move(board);
+            ghost.move(board, pacman.x, pacman.y);
 
-            System.out.println("Score: " + score);
-            System.out.println("-------------------");
+            System.out.println("-------------------------");
         }
 
+        System.out.println("Final Score: " + score);
+        System.out.println("Thanks for playing!");
         sc.close();
     }
 }
