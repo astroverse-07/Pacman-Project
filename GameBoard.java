2feee93bda06497452ac7fc52d5e615023522cdd
Abdullah Ghafoor <143638785+astroverse-07@users.noreply.github.com>
2026-05-13 21:19:27 +0500

    Update GameBoard.java

--- GameBoard.java
+++ GameBoard.java
@@ -1,18 +1,23 @@
-import java.util.Random;
-
 public class GameBoard {
     int rows = 10;
     int cols = 10;
     char[][] board = new char[rows][cols];
+    boolean[][] walls = new boolean[rows][cols];
 
     public GameBoard() {
         initializeBoard();
     }
 
+    public void addWall(int x, int y) {
+        if (x >= 0 && x < rows && y >= 0 && y < cols) {
+            walls[x][y] = true;
+        }
+    }
+
     public void initializeBoard() {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                board[i][j] = '.';
+                board[i][j] = walls[i][j] ? '#' : '.';
             }
         }
     }
@@ -31,6 +36,6 @@ public class GameBoard {
     }
 
     public boolean isValid(int x, int y) {
-        return x >= 0 && x < rows && y >= 0 && y < cols;
+        return x >= 0 && x < rows && y >= 0 && y < cols && !walls[x][y];
     }
 }
